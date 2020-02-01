6ca7f89dc00b3c647801cb225075e4b18f7e494f
Romain Durade <durade.romain@gmail.com>
2020-02-01 13:45:00 +0100

    add some aliases

--- aliases.sh
+++ aliases.sh
@@ -2,7 +2,19 @@ command_exists () {
     type "$1" &> /dev/null ;
 }
 
+# safety nets
+# do not delete / or prompt if deleting more than 3 files at a time #
+alias rm='rm -I --preserve-root'
+# confirmation
+alias mv='mv -i'
+alias cp='cp -i'
+alias ln='ln -i'
+# parenting changing perms on / #
+alias chown='chown --preserve-root'
+alias chmod='chmod --preserve-root'
+alias chgrp='chgrp --preserve-root'
 
+# vscodium === vscode
 if command_exists codium; then
     alias code=codium
 fi
